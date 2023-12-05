489984c99caa0988b7c6baf041881ceef277d508
Younes-Hounain <younesho9@outlook.com>
2023-12-05 21:51:37 +0100

    [CD] : Added JSON Database file and fetch :package:

--- Product-Management-App/src/app/products/products.component.ts
+++ Product-Management-App/src/app/products/products.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
 
+  constructor(private http:HttpClient) {
+  }
+
+
+
+  products : Array<any> = [];
+
+  handleCheckProduct(product: any) {
+    product.checked = !product.checked;
+  }
+
+  ngOnInit(): void {
+    this.http.get<Array<any>>("http://localhost:8089/products").subscribe((data: any) => {
+      this.products = data;
+    },
+      (error: any) => {
+        console.log(error);
+      });
+  }
 }
