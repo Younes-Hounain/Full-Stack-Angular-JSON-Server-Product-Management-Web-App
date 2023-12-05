399798d5ce7033a3f481c1a09dd611e938a9cf45
Younes-Hounain <younesho9@outlook.com>
2023-12-05 20:23:59 +0100

    [CD] : Added Routing and NavBar :heavy_plus_sign:

---
+++ emsi-app/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterLink, RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  actions : Array<any> = [
+    {title: 'Home', route:"/home", icon: 'house'},
+    {title: 'Products', route:"/products", icon: 'search'},
+    {title: 'New Product', route:"/newProduct", icon: 'safe'},
+  ]
+  currentAction: any;
+
+  setCurrentAction(action: any) {
+    this.currentAction = action;
+  }
+}
